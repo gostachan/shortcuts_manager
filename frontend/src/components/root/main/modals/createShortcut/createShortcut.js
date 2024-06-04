82073f2821c9f2fdb658cd97951d52030d7dd111
Hara Misaki <m.kevavu05@gmail.com>
2024-06-04 23:29:48 +0900

    Implement: create shortcut modal

---
+++ frontend/src/components/root/main/modals/createShortcut/createShortcut.js
@@ -0,0 +1,12 @@
+import "./createShortcut.css"
+
+export default function CreateShortcut({props}) {
+
+  return (
+    <div id="createShortcutModal">
+      <div className="container">
+
+      </div>
+    </div>
+  );
+}

12dc83717ab0a74d21fe20649013e97b19615033
Hara Misaki <m.kevavu05@gmail.com>
2024-06-06 15:37:19 +0900

    refactor: fix sidbar.js and shortcutTable.js

--- frontend/src/components/root/main/shortcutTable/shortcutTable.js
+++ frontend/src/components/root/main/shortcutTable/shortcutTable.js
@@ -2,95 +2,51 @@ import "./shortcutTable.css";
 import ShortcutTableRecord from "./shortcutTableRecord/shortcutTableRecord";
 
 export default function ShortcutTable() {
+  let columns = ["favorite", "command", "keybinding", "when", "environment"]
+  let valueSets = [{favorite:   false,
+                 command:    "command1",
+                 keybinding: "keybinding1",
+                 when:       "when1",
+                 environment: "environment1"
+                },
+                {favorite:   false,
+                 command:    "command2",
+                 keybinding: "keybinding2",
+                 when:       "when2",
+                 environment: "environment2"
+                },
+                {favorite:   false,
+                 command:    "command3",
+                 keybinding: "keybinding3",
+                 when:       "when3",
+                 environment: "environment3"
+                },
+                {favorite:   false,
+                 command:    "command4",
+                 keybinding: "keybinding4",
+                 when:       "when4",
+                 environment: "environment4"
+                }]
 
   return (
     <div className="shortcut-table">
       <div className="record record-first">
-        <div className="column-1">
-          <h5>Favorite</h5>
-        </div>
-        <div className="column-2">
-          <h5>Command</h5>
-        </div>
-        <div className="column-3">
-          <h5>Keybiding</h5>
-        </div>
-        <div className="column-4">
-          <h5>When</h5>
-        </div>
-        <div className="column-5">
-          <h5>Environment</h5>
-        </div>
+        {columns.map((column, index) => {
+          return (
+          <div className={`column-${index + 1}`}>
+            <h5>{column}</h5>
+          </div>
+          )
+        })}
       </div>
-
-      <ShortcutTableRecord props={{value :{favorite: true, 
-                                   command: "コマンド", 
-                                   keybinding: "ctrl j",
-                                   when: "when focuson hoge",
-                                   environment: "hoge"},
-                                   className: ""}} />
-      <ShortcutTableRecord props={{value :{favorite: true, 
-                                   command: "コマンド", 
-                                   keybinding: "ctrl j",
-                                   when: "when focuson hoge",
-                                   environment: "hoge"},
-                                   className: ""}} />
-      <ShortcutTableRecord props={{value :{favorite: true, 
-                                   command: "コマンド", 
-                                   keybinding: "ctrl j",
-                                   when: "when focuson hoge",
-                                   environment: "hoge"},
-                                   className: ""}} />
-      <ShortcutTableRecord props={{value :{favorite: true, 
-                                   command: "コマンド", 
-                                   keybinding: "ctrl j",
-                                   when: "when focuson hoge",
-                                   environment: "hoge"},
-                                   className: ""}} />
-      <ShortcutTableRecord props={{value :{favorite: true, 
-                                   command: "コマンド", 
-                                   keybinding: "ctrl j",
-                                   when: "when focuson hoge",
-                                   environment: "hoge"},
-                                   className: ""}} />
-      <ShortcutTableRecord props={{value :{favorite: true, 
-                                   command: "コマンド", 
-                                   keybinding: "ctrl j",
-                                   when: "when focuson hoge",
-                                   environment: "hoge"},
-                                   className: ""}} />
-      <ShortcutTableRecord props={{value :{favorite: true, 
-                                   command: "コマンド", 
-                                   keybinding: "ctrl j",
-                                   when: "when focuson hoge",
-                                   environment: "hoge"},
-                                   className: ""}} />
-      <ShortcutTableRecord props={{value :{favorite: true, 
-                                   command: "コマンド", 
-                                   keybinding: "ctrl j",
-                                   when: "when focuson hoge",
-                                   environment: "hoge"},
-                                   className: ""}} />
-      <ShortcutTableRecord props={{value :{favorite: true, 
-                                   command: "コマンド", 
-                                   keybinding: "ctrl j",
-                                   when: "when focuson hoge",
-                                   environment: "hoge"},
-                                   className: ""}} />
-      <ShortcutTableRecord props={{value :{favorite: true, 
-                                   command: "コマンド", 
-                                   keybinding: "ctrl j",
-                                   when: "when focuson hoge",
-                                   environment: "hoge"},
-                                   className: ""}} />
-      <ShortcutTableRecord props={{value :{favorite: true, 
-                                   command: "コマンド", 
-                                   keybinding: "ctrl j",
-                                   when: "when focuson hoge",
-                                   environment: "hoge"},
-                                   className: "record-last"}} />
-
-
+      {valueSets.map((valueSet, index) => {
+        return (
+          <ShortcutTableRecord props={{value: valueSet,
+                                      className: ((index === valueSets.length - 1) ? "record-last" : "")
+          }}
+          />
+        )
+      })}
     </div>
   );
 }

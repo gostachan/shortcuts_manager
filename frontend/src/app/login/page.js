7fd6498875527949b284cd0b934d4ca8126a0b0f
Hara Misaki <m.kevavu05@gmail.com>
2024-06-06 20:55:48 +0900

    Implement: login and signup page

--- frontend/src/app/login/page.js
+++ frontend/src/app/login/page.js
@@ -1,6 +1,40 @@
+import "./page.css";
+
 export default function Login() {
 
   return (
-    <h1>log in</h1>
+    <div className="container">
+      <div className="login-form">
+        <div className="content">
+          <form action="/login" method="post">
+            <h1 className="form-title">log in</h1>
+            <div className="form-group">
+              <input
+                type="text"
+                id="email"
+                name="email"
+                required=""
+                placeholder="email"
+              />
+            </div>
+            <div className="form-group">
+              <input
+                type="password"
+                id="password"
+                name="password"
+                required=""
+                placeholder="password"
+              />
+            </div>
+            <div className="forget-pass">
+              <a href="#">パスワードを忘れた</a>
+            </div>
+            <div className="form-button form-group">
+              <button type="submit">log in</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
   )
 }
--- frontend/src/app/signup/page.js
+++ frontend/src/app/signup/page.js
@@ -1,6 +1,47 @@
-export default function Login() {
+import "./page.css"
+
+export default function Signup() {
 
   return (
-    <h1>sign up</h1>
+    <div className="container">
+      <div className="login-form">
+        <div className="content">
+          <form action="/signup" method="post">
+            <h1 className="form-title">log in</h1>
+            <div className="form-group">
+              <input
+                type="text"
+                id="email"
+                name="email"
+                required=""
+                placeholder="email"
+              />
+            </div>
+            <div className="form-group">
+              <input
+                type="password"
+                id="password"
+                name="password"
+                required=""
+                placeholder="password"
+              />
+            </div>
+            <div className="form-group">
+              <input
+                type="password"
+                id="password-confirmation"
+                name="password-confirmation"
+                required=""
+                placeholder="password confirmation"
+              />
+            </div>
+            <div className="form-button form-group">
+              <button type="submit">sign up</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+
   )
 }
